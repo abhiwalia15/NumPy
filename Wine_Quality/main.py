b3b417b19f79f5bd342e1f89e427e8d1af9f4e60
Mrinal <waliamrinal@gmail.com>
2018-09-11 00:18:01 +0530

    creating a random no. array in numpy

--- Wine_Quality/main.py
+++ Wine_Quality/main.py
@@ -5,5 +5,8 @@ qualities = [float(item[-1]) for item in wines[1:]]
 print(sum(qualities) / len(qualities))
 
 wines = np.array(wines[1:], dtype = np.float)
+wine = np.genfromtxt("winequality-red.csv", delimiter=";", skip_header=1)
 print(wines)
-print(wines.shape)
+print(wine)
+
+#print(wines.shape)
--- random_item_array.py
+++ random_item_array.py
@@ -2,5 +2,7 @@ import numpy as np
 
 #create an array of random items
 rand = np.random.rand(3,4)
+int_type = rand.astype(int)
 
 print(rand)
+print(int_type)
