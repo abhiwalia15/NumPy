81548f58930c82018eb520751283639746c202da
Mrinal <waliamrinal@gmail.com>
2018-09-11 00:19:51 +0530

    basic arth. operations using numpy

---
+++ arthametic_operations.py
@@ -0,0 +1,15 @@
+import numpy as np
+#this all dosen't change the values of a permanentely.
+
+a = np.random.rand(3,3)
+
+print(a)
+
+print(a[:,2] + 10)
+a[2,2] - 8
+a[1] * 2
+a[0]**2
+a.astype(float)
+a[2]/3
+
+a[:,0]+a[0,:]
