867de547e86637d582966ec9f549a41bf8de34ca
Mrinal <waliamrinal@gmail.com>
2018-09-10 17:19:08 +0530

    basic operations in numpy

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+a=np.array([[1,2,4],[1,4,3]]) #2-d array
+b=np.array([2,3,4])
+
+a.dtype #to know the datatype
+a.itemsize #byte size
+a.ndim #to know dimension
+a.size #to know no. of elements
+a.shape #(row,columns)
